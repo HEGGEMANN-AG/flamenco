7a04b2439bb294e4f45c9b2523107448b90a9a30
Niclas Klugmann <niclas@klugmann.de>
2026-03-09 18:41:01 +0100

    better netbios module

--- src/netbios.rs
+++ src/netbios.rs
@@ -10,7 +10,9 @@ pub struct NetBiosContent([u8]);
 impl ToOwned for NetBiosContent {
     type Owned = OwnedNetBios;
     fn to_owned(&self) -> Self::Owned {
-        OwnedNetBios::new(&self.0).unwrap()
+        let buf: Box<[u8]> = self.0.into();
+        let neg_box = unsafe { std::mem::transmute::<Box<[u8]>, Box<NetBiosContent>>(buf) };
+        OwnedNetBios(neg_box)
     }
 }
 impl NetBiosContent {
@@ -32,34 +34,29 @@ impl NetBiosContent {
 pub fn use_as_netbios_content(slice: &[u8]) -> Option<&NetBiosContent> {
     check_size(slice.len())
         .is_some()
-        .then_some(unsafe { std::mem::transmute::<&[u8], &NetBiosContent>(slice) })
+        .then_some(unsafe { use_as_netbios_content_unchecked(slice) })
+}
+unsafe fn use_as_netbios_content_unchecked(slice: &[u8]) -> &NetBiosContent {
+    unsafe { std::mem::transmute::<&[u8], &NetBiosContent>(slice) }
 }
 
 fn check_size(s: usize) -> Option<u32> {
     u32::try_from(s).ok().filter(|len| *len < CRITICAL_SIZE)
 }
 
-pub struct OwnedNetBios(Box<[u8]>);
+pub struct OwnedNetBios(Box<NetBiosContent>);
 impl OwnedNetBios {
-    pub fn new(slice: &[u8]) -> Option<Self> {
-        let length = check_size(slice.len())?;
-        let be_len = length.to_be_bytes();
-        assert_eq!(be_len[0], 0);
-        let mut buf = Vec::with_capacity(4 + slice.len());
-        buf.extend(be_len);
-        buf.extend_from_slice(slice);
-        Some(Self(buf.into_boxed_slice()))
-    }
     pub async fn read_from_async<R: AsyncRead + Unpin>(r: &mut R) -> Result<Self, ReadError> {
         let mut bios_size = [0u8; 4];
         r.read_exact(&mut bios_size).await.map_err(ReadError::Io)?;
         let Some(size) = check_size(u32::from_be_bytes(bios_size) as usize) else {
             return Err(ReadError::InvalidLength);
         };
-        let mut buf = vec![0; 4 + size as usize];
-        buf[..4].copy_from_slice(&bios_size);
-        r.read_exact(&mut buf[4..]).await.map_err(ReadError::Io)?;
-        Ok(Self(buf.into_boxed_slice()))
+        let mut buf = vec![0; size as usize];
+        r.read_exact(&mut buf).await.map_err(ReadError::Io)?;
+        Ok(Self(unsafe {
+            std::mem::transmute::<Box<[u8]>, Box<NetBiosContent>>(buf.into_boxed_slice())
+        }))
     }
 }
 impl Borrow<NetBiosContent> for OwnedNetBios {
@@ -70,7 +67,7 @@ impl Borrow<NetBiosContent> for OwnedNetBios {
 impl Deref for OwnedNetBios {
     type Target = NetBiosContent;
     fn deref(&self) -> &Self::Target {
-        use_as_netbios_content(&self.0[4..]).expect("checked on creation")
+        &self.0
     }
 }
 
@@ -78,10 +75,4 @@ impl Deref for OwnedNetBios {
 pub enum ReadError {
     Io(std::io::Error),
     InvalidLength,
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test() {}
 }
