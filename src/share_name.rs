e69bb40ed0aceaa1e03d20d4657dd07a2da083a3
Niclas Klugmann <niclas@klugmann.de>
2026-04-27 22:15:07 +0200

    refactor

--- src/share_name.rs
+++ src/share_name.rs
@@ -21,7 +21,7 @@ impl ShareName {
 fn validate_share_name(s: &str) -> Result<(), InvalidShareName> {
     s.chars().try_fold(0usize, |count, next_char| match next_char {
         c if SHARE_NAME_ILLEGAL_CHARACTERS.contains(&c) => Err(InvalidShareName::InvalidCharacter(c)),
-        c if ('\0'..='\u{1F}').contains(&c) => Err(InvalidShareName::InvalidControlCharacter),
+        '\0'..='\u{1F}' => Err(InvalidShareName::InvalidControlCharacter),
         _ if count > 80 => Err(InvalidShareName::TooLong),
         _ => Ok(count + 1),
     })?;
